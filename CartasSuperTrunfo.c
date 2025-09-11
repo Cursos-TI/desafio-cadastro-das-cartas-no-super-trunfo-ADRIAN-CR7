30bb96e11aa4fa84ab71074e8d78b60ec75de1b0
ADRIAN-CR7 <adrianlima2020.com@gmail.com>
2025-09-11 09:42:43 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     char cidade1[50], cidade2[50];
-    int populacao1, populacao2;
+    int populacao1, populacao2, codigo1, codigo2;
     float area1, area2;
     float pib1, pib2;
     int p_turisticos1, p_turisticos2;
+    float densipopu1, densipopu2;
+    float pibpercapita1, pibpercapita2;
 
     printf("Digite o nome da primeira cidade:\n");
     fgets(cidade1, sizeof(cidade1), stdin);
-    cidade1[strcspn(cidade1, "\n")] = '\0';  // Remove o \n do final
+    cidade1[strcspn(cidade1, "\n")] = '\0'; 
+    
+    printf("Digite o seu código:\n");
+    scanf("%d", &codigo1);
 
     printf("Digite a populacao:\n");
     scanf("%d", &populacao1);
@@ -29,11 +29,14 @@ int main() {
     printf("Digite a quantidade de pontos turisticos:\n");
     scanf("%d", &p_turisticos1);
 
-    getchar(); // Limpa o \n que ficou no buffer depois do último scanf
-
+    getchar(); // limpa o Enter pendente
+    
     printf("\nDigite o nome da segunda cidade:\n");
     fgets(cidade2, sizeof(cidade2), stdin);
-    cidade2[strcspn(cidade2, "\n")] = '\0';  // Remove o \n do final
+    cidade2[strcspn(cidade2, "\n")] = '\0'; 
+    
+    printf("Digite o seu código:\n");
+    scanf("%d", &codigo2);
 
     printf("Digite a populacao:\n");
     scanf("%d", &populacao2);
@@ -47,17 +50,24 @@ int main() {
     printf("Digite a quantidade de pontos turisticos:\n");
     scanf("%d", &p_turisticos2);
 
-    // Imprime só para conferir se leu tudo certo:
+    // cálculos corretos
+    densipopu1 = (float) populacao1 / area1;
+    densipopu2 = (float) populacao2 / area2;
+    pibpercapita1 = (float) pib1 / populacao1;
+    pibpercapita2 = (float) pib2 / populacao2;
+
     printf("\nDados da primeira cidade:\n");
-    printf("Nome: %s\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\nPontos Turisticos: %d\n",
-        cidade1, populacao1, area1, pib1, p_turisticos1);
+    printf("Nome: %s\nCodigo: %d\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\nPontos Turisticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per capita: %.2f\n",
+        cidade1, codigo1, populacao1, area1, pib1, p_turisticos1, densipopu1, pibpercapita1);
 
     printf("\nDados da segunda cidade:\n");
-    printf("Nome: %s\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\nPontos Turisticos: %d\n",
-        cidade2, populacao2, area2, pib2, p_turisticos2);
-        
+    printf("Nome: %s\nCodigo: %d\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\nPontos Turisticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per capita: %.2f\n",
+        cidade2, codigo2, populacao2, area2, pib2, p_turisticos2, densipopu2, pibpercapita2);
 
     return 0;
 }
+
+
+
 
 
